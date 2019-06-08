2b9f186853e121bdc7920949822ba4f3ad269a75
Trey <trey.hamilton21@gmail.com>
2019-06-08 15:42:41 -0500

    blog post form

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import ListView
-from blog.models import BlogPost
+from blog.models import BlogPost, BlogPostForm
 
 
 def search(request):
@@ -16,6 +16,18 @@ def search(request):
         return render(request, 'search_results.html', {})
 
 
+def blog_post_form(request):
+    form = BlogPostForm()
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+        else:
+            print('FORM ERROR')
+
+    return render(request, 'blog_post_form.html', {'form': form})
+
+
 class Home(ListView):
     context_object_name = 'recent_posts'
     queryset = BlogPost.objects.filter(id__lte=5)
@@ -25,22 +37,22 @@ class Home(ListView):
 class ProgrammingBlogPosts(ListView):
     context_object_name = 'prog_posts'
     queryset = BlogPost.objects.filter(topic_type='PROG')
-    # template_name = porgramming.html
+    template_name = 'programming.html'
 
 
 class WebDevelopmentBlogPosts(ListView):
     context_object_name = 'web_posts'
     queryset = BlogPost.objects.filter(topic_type='WEB')
-    # template_name = web.html
+    template_name = 'web.html'
 
 
 class DataScienceAIBlogPosts(ListView):
     context_object_name = 'data_posts'
     queryset = BlogPost.objects.filter(topic_type='DATA')
-    # template_name = dataai.html
+    template_name = 'data_ai.html'
 
 
 class GeneralBlogPosts(ListView):
     context_object_name = 'gen_posts'
     queryset = BlogPost.objects.filter(topic_type='GEN')
-    # template_name = general.html
+    template_name = 'general.html'
